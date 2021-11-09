99a1bc0ff856dd8a15597bed462d913c1ea0e868
longshot-47 <93192652+longshot-47@users.noreply.github.com>
2021-11-09 18:44:05 +0800

    Add files via upload

--- A B C.py
+++ A B C.py
@@ -2,8 +2,14 @@ A = input("number of A: ")
 B = input("number of B: ")
 C = input("number of C: ")
 largest = A
+smallest = B
 if B > largest:
     largest = B
 elif C > B > largest and C > largest > B:
     largest = C
-print("the largest number is "+largest)
+if A < smallest:
+    smallest = A
+elif C < smallest and C < A < smallest:
+    smallest = C
+print("the largest number is: "+largest)
+print("the smallest number is: "+smallest)
