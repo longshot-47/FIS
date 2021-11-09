6d81df642f5f29b1bcf046fdbe4f2087187c077e
longshot-47 <93192652+longshot-47@users.noreply.github.com>
2021-11-09 18:35:27 +0800

    Add files via upload

--- A B C.py
+++ A B C.py
@@ -1,9 +1,9 @@
-A = input("number of A")
-B = input("number of B")
-C = input("number of C")
+A = input("number of A: ")
+B = input("number of B: ")
+C = input("number of C: ")
 largest = A
 if B > largest:
     largest = B
-elif C > largest:
+elif C > B > largest and C > largest > B:
     largest = C
-print(largest)
+print("the largest number is "+largest)
